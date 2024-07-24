68cbac5b8a810427f891837c332633207ffa2bf8
John William <johnwilliams1756@gmail.com>
2024-07-24 22:56:55 +0300

    tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,17 +5,32 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
       <Tabs.Screen
-        name="home"
+        name="Home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="discover"
+        name="Discover"
         options={{
           title: 'Discover',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="search" color={color} />,
+        }}
+      />
+
+<Tabs.Screen
+        name="Saved"
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="bookmark" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="search" color={color} />,
         }}
       />
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
 import { Redirect } from "expo-router";
 
-
 export default function Index() {
   return (
-    <Redirect href='/home'/>
+    <Redirect href='/Home'/>
   )
 }
